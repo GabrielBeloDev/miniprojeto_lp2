bac81f20c92a945075f6e6306ff20afdf2d98db1
GabrielBeloDev <gabrielbelodev@outlook.com>
2023-10-05 12:32:13 -0300

    just a few updates

--- src/applications/Main.java
+++ src/applications/Main.java
@@ -1,22 +1,45 @@
 package applications;
 
 import entities.Biblioteca;
+import entities.Item;
 import entities.Livro;
 import entities.Revista;
 
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
 
-        Livro livro = new Livro("harry", "rowling","magia","x",1900,10,"m");
+        Livro livro = new Livro("Harry Potter", "J.K. Rowling", "Livro sobre magia", "Editora X", 1998, 500, "Fantasia");
         biblioteca.addItem(livro);
+        System.out.println("Livro add: "+ livro.getTitulo());
+        System.out.println();
 
         Revista revista = new Revista("Manga Y", "Autor A", "Manga sobre heróis", "Editora Y", 2020, 100, "Manga", 5, "Maio");
         biblioteca.addItem(revista);
+        System.out.println("Revista add: "+ revista.getAutor());
+        System.out.println();
 
 
+        ArrayList<Item> resultadosTitulo = biblioteca.pesquisaPorTitulo("Harry");
+        System.out.println("Resultados da pesquisa por título:");
+        for (Item item : resultadosTitulo) {
+            System.out.println(item.getTitulo());
+        }
 
+        // Testando a pesquisa por autor
+        ArrayList<Item> resultadosAutor = biblioteca.pesquisaPorAutor("J.K. Rowling");
+        System.out.println("\nResultados da pesquisa por autor:");
+        for (Item item : resultadosAutor) {
+            System.out.println(item.getAutor());
+        }
 
-
+        // Testando a pesquisa por resumo
+        ArrayList<Item> resultadosResumo = biblioteca.pesquisaPorResumo("magia");
+        System.out.println("\nResultados da pesquisa por resumo:");
+        for (Item item : resultadosResumo) {
+            System.out.println(item.getResumo());
+        }
     }
 }
--- src/entities/Biblioteca.java
+++ src/entities/Biblioteca.java
@@ -8,4 +8,45 @@ public class Biblioteca {
     public void addItem(Item item) {
         acervo.add(item);
     }
+
+
+    public ArrayList<Item> pesquisaPorTitulo(String palavra) {
+        ArrayList<Item> resultados = new ArrayList<>();
+        for(Item item : acervo){
+            if(item.getTitulo().contains(palavra)){
+                resultados.add(item);
+            }
+
+        }
+        return resultados;
+    }
+
+    public ArrayList<Item> pesquisaPorAutor(String palavra) {
+        ArrayList<Item> resultados = new ArrayList<>();
+        for(Item item : acervo){
+            if(item.getAutor().contains(palavra)){
+                resultados.add(item);
+            }
+
+        }
+        return resultados;
+    }
+
+    public ArrayList<Item> pesquisaPorResumo(String palavra) {
+        ArrayList<Item> resultados = new ArrayList<>();
+        for(Item item : acervo){
+            if(item.getResumo().contains(palavra)){
+                resultados.add(item);
+            }
+
+        }
+        return resultados;
+    }
+
+
+
+
+
 }
+
+
