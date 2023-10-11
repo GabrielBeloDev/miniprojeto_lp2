e47ff09dff5f1a0acccc2097489ceda8e9dfffda
GabrielBeloDev <gabrielbelodev@outlook.com>
2023-10-11 02:21:42 -0300

    creating a new class

---
+++ src/entities/Usuario.java
@@ -0,0 +1,52 @@
+package entities;
+
+public class Usuario {
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+    private boolean isAdulto;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public boolean isAdulto() {
+        return isAdulto;
+    }
+
+    public void setAdulto(boolean adulto) {
+        isAdulto = adulto;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", dataNascimento='" + dataNascimento + '\'' +
+                ", isAdulto=" + isAdulto +
+                '}';
+    }
+}
