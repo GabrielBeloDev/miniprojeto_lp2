191380962f74dcc5c1b873fe4617293b3b7bed72
GabrielBeloDev <gabrielbelodev@outlook.com>
2023-10-15 12:33:51 -0300

    fixing some mistakes

--- src/entities/Revista.java
+++ src/entities/Revista.java
@@ -1,12 +1,29 @@
 package entities;
 
-public class Revista extends Item{
+public class Revista extends Item {
     private int volume;
 
     private String mesPublicacao;
+
     public Revista(String titulo, String autor, String resumo, String editora, int ano, int qntdePaginas, String genero, int volume, String mesPublicacao) {
         super(titulo, autor, resumo, editora, ano, qntdePaginas, genero);
         this.volume = volume;
+        this.mesPublicacao = mesPublicacao;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getMesPublicacao() {
+        return mesPublicacao;
+    }
+
+    public void setMesPublicacao(String mesPublicacao) {
         this.mesPublicacao = mesPublicacao;
     }
 }
